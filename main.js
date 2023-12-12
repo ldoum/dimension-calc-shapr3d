805ba5218db17e6ca5f287c9e1bd691851e34230
Lancine Doumbia <107377652+ldoum@users.noreply.github.com>
2023-12-12 15:38:42 -0800

    Create main.js
    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const {lexer} = require("./calc_logic/lexical.js");
+const {clarifier} = require("./calc_logic/syntactical.js");
+const {parser} = require("./calc_logic/postfix.js");
+const {solver} = require("./calc_logic/mathcore.js");
+
+let format = "in";
+
+//INPUT
+var input = "5+10*3*3-12"; //return 5
+
+//////CONNECT ALL FUNCTIONS///////////
+function user(equation_expr) {
+  const lex = lexer(equation_expr,format);
+  const gramm = clarifier(lex, format);
+  const parse = parser(gramm);
+  return solver(parse);
+}
+////////////////////////////
+
+console.log(`\nThe answer is: ${user(input)} ${format}`);
